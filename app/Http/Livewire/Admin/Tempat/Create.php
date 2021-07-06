8319fc4186f9d3ab5138109a3bc9784ceabe383a
viantotech <viantotech@users.noreply.github.com>
2021-07-06 07:00:19 +0700

    Fungsi Web
    Web

---
+++ app/Http/Livewire/Admin/Tempat/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Tempat;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.tempat.create');
+    }
+}
---
+++ app/Http/Livewire/User/Daftarvaksin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Daftarvaksin extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.daftarvaksin');
+    }
+}
